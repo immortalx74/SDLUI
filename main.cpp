4cf9ac482890d63c143a61ce81806d73dad81c78
immortalx74 <jfdodis@yahoo.com>
2020-07-07 06:22:32 +0300

    SDLUI prefix

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ int main(int argc, char *argv[])
 	SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("ui", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    ui_Init(renderer, window);
+    SDLUI_Init(renderer, window);
 	bool quit = false;
 	
-    BUTTON *btn = ui_CreateButton("PushButton", 20, 100);
-    SLIDER_INT *si = ui_CreateSliderInt(30, 30, 0, 100, 50);
+    SDLUI_Control_Button *btn = SDLUI_CreateButton("PushButton", 20, 200);
+    SDLUI_Control_SliderInt *sl1 = SDLUI_CreateSliderInt(20, 20, 0, 100, 50, SDLUI_ORIENTATION_VERTICAL);
+    SDLUI_Control_SliderInt *sl2 = SDLUI_CreateSliderInt(80, 20, 0, 100, 50);
     
     SDL_Event e;
     
@@ -24,22 +25,32 @@ int main(int argc, char *argv[])
 	            quit = true;
 	        }
             
-            ui_MouseStateSet(e.type, e.button.button);
+            SDLUI_MouseStateSet(e.type, e.button.button);
         }
         
         
-        if(ui_Button(btn))
+        if(SDLUI_Button(btn))
         {
-            std::cout << "here" << std::endl;
+            
+        }
+        
+        if(SDLUI_SliderInt(sl1))
+        {
+            
+        }
+        
+        if(SDLUI_SliderInt(sl2))
+        {
+            
         }
         
         SDL_SetRenderDrawColor(renderer, 60, 60, 60, 255);
         SDL_RenderClear(renderer);
-        ui_Render();
+        SDLUI_Render();
         SDL_RenderPresent(renderer);
     }
     
-    free(ui_base.pool);
+    free(SDLUI_Base.pool);
     TTF_Quit();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
--- ui.h
+++ ui.h
@@ -1,6 +1,8 @@
 #pragma once
+
 #include <iostream>
 #include "SDL_ttf.h"
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -10,38 +12,52 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
-#define MAX_CONTROLS 50
+#define SDLUI_MAX_CONTROLS 50
 
-enum ALIGN
+// Enums ----------------------------------------------
+
+enum SDLUI_ALIGN
 {
-    LEFT = 0,
-    CENTER = 1,
-    RIGHT = 2
+    SDLUI_ALIGN_LEFT = 0,
+    SDLUI_ALIGN_CENTER = 1,
+    SDLUI_ALIGN_RIGHT = 2
 };
 
-enum MOUSE_STATE
+enum SDLUI_ORIENTATION
 {
-    NONE,
-    PRESSED,
-    HELD,
-    RELEASED
+    SDLUI_ORIENTATION_HORIZONTAL,
+    SDLUI_ORIENTATION_VERTICAL
 };
 
-enum CONTROL_TYPE
+enum SDLUI_MOUSEBUTTON
 {
-    Button,
-    SliderInt
+    SDLUI_MOUSEBUTTON_NONE,
+    SDLUI_MOUSEBUTTON_PRESSED,
+    SDLUI_MOUSEBUTTON_HELD,
+    SDLUI_MOUSEBUTTON_RELEASED
 };
 
-struct THEME
+enum SDLUI_CONTROL_TYPE
+{
+    SDLUI_CONTROL_TYPE_BUTTON,
+    SDLUI_CONTROL_TYPE_SLIDER_INT
+};
+
+
+// Structs --------------------------------------------
+
+struct SDLUI_Theme
 {
     SDL_Color col_base = {75, 75, 75, 255};
     SDL_Color col_border = {38, 38, 38, 255};
     SDL_Color col_hover = {80, 80, 80, 255};
     SDL_Color col_click = {40, 40, 40, 255};
+    SDL_Color col_highlight = {65, 105, 225, 255};
+    SDL_Color col_test = {255, 0, 225, 255};
+    SDL_Color col_thumb = {66, 66, 66, 255};
 };
 
-struct BUTTON
+struct SDLUI_Control_Button
 {
     i32 x;
     i32 y;
@@ -49,12 +65,12 @@ struct BUTTON
     i32 h;
     char *caption;
     bool render;
-    ALIGN align;
+    SDLUI_ALIGN align;
     SDL_Texture *t_text;
     SDL_Texture *t_back;
 };
 
-struct SLIDER_INT
+struct SDLUI_Control_SliderInt
 {
     i32 x;
     i32 y;
@@ -63,20 +79,24 @@ struct SLIDER_INT
     i32 min;
     i32 max;
     i32 value;
+    SDLUI_ORIENTATION orientation;
+    i32 thumb_size;
+    bool render;
+    bool ischanging;
 };
 
-struct CONTROL
+struct SDLUI_Control
 {
-    CONTROL_TYPE type;
+    SDLUI_CONTROL_TYPE type;
     
     union
     {
-        BUTTON button;
-        SLIDER_INT slider_int;
+        SDLUI_Control_Button button;
+        SDLUI_Control_SliderInt slider_int;
     };
 };
 
-struct UI_FONT
+struct SDLUI_Font
 {
     TTF_Font *handle;
     i32 size = 16;
@@ -84,7 +104,7 @@ struct UI_FONT
     i32 height;
 };
 
-struct UI_BASE
+struct __SDLUI_Base
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -92,16 +112,32 @@ struct UI_BASE
     i32 window_height;
     
     i32 count = 0;
-    CONTROL *pool;
+    SDLUI_Control *pool;
     
     u8 mouse_current_frame[5] = {0};
     u8 mouse_last_frame[5] = {0};
-    THEME theme;
-    UI_FONT font;
+    SDLUI_Theme theme;
+    SDLUI_Font font;
     
-}ui_base;
+}SDLUI_Base;
 
-bool ui_PointCollision(SDL_Rect rect, i32 x, i32 y)
+
+// Functions ----------------------------------------------
+
+float SDLUI_Map(float in_min, float in_max, float out_min, float out_max, float value)
+{
+    float slope = (out_max - out_min) / (in_max - in_min);
+    return out_min + (slope * (value - in_min));
+}
+
+float SDLUI_Clamp(float n, float min, float max)
+{
+    if (n < min) n = min;
+    if (n > max) n = max;
+    return n;
+}
+
+bool SDLUI_PointCollision(SDL_Rect rect, i32 x, i32 y)
 {
     if(x >= rect.x && x <= rect.x + rect.w &&
        y >= rect.y && y <= rect.y +rect.h)
@@ -111,68 +147,69 @@ bool ui_PointCollision(SDL_Rect rect, i32 x, i32 y)
     return false;
 }
 
-void ui_Init(SDL_Renderer *r, SDL_Window *w)
+void SDLUI_Init(SDL_Renderer *r, SDL_Window *w)
 {
     TTF_Init();
-    ui_base.font.handle = TTF_OpenFont("liberation-mono.ttf", ui_base.font.size);
-    ui_base.font.height = TTF_FontHeight(ui_base.font.handle);
-    TTF_SizeText(ui_base.font.handle, "0", &ui_base.font.width, &ui_base.font.height);
+    SDLUI_Base.font.handle = TTF_OpenFont("liberation-mono.ttf", SDLUI_Base.font.size);
+    SDLUI_Base.font.height = TTF_FontHeight(SDLUI_Base.font.handle);
+    TTF_SizeText(SDLUI_Base.font.handle, "0", &SDLUI_Base.font.width, &SDLUI_Base.font.height);
     
     
-    ui_base.renderer = r;
-    ui_base.window = w;
-    SDL_GetWindowSize(ui_base.window, &ui_base.window_width, &ui_base.window_height);
-    ui_base.pool = (CONTROL*)malloc(MAX_CONTROLS * sizeof(CONTROL));
+    SDLUI_Base.renderer = r;
+    SDLUI_Base.window = w;
+    SDL_GetWindowSize(SDLUI_Base.window, &SDLUI_Base.window_width, &SDLUI_Base.window_height);
+    SDLUI_Base.pool = (SDLUI_Control*)malloc(SDLUI_MAX_CONTROLS * sizeof(SDLUI_Control));
 }
 
-void ui_MouseStateReset()
+void SDLUI_MouseStateReset()
 {
     for (int i = 0; i < 5; ++i)
     {
-        ui_base.mouse_last_frame[i] = ui_base.mouse_current_frame[i];
+        SDLUI_Base.mouse_last_frame[i] = SDLUI_Base.mouse_current_frame[i];
     }
 }
 
-void ui_MouseStateSet(i32 event_type, i32 mbutton)
+void SDLUI_MouseStateSet(i32 event_type, i32 mbutton)
 {
-    if(event_type == SDL_MOUSEBUTTONDOWN)
-    {
-        ui_base.mouse_current_frame[mbutton] = 1;
-    }
-    else
-    {
-        ui_base.mouse_current_frame[mbutton] = 0;
-    }
+    //if(event_type == SDL_MOUSEBUTTONDOWN)
+    //{
+    //SDLUI_Base.mouse_current_frame[mbutton] = 1;
+    //}
+    //else
+    //{
+    //SDLUI_Base.mouse_current_frame[mbutton] = 0;
+    //}
+    SDLUI_Base.mouse_current_frame[mbutton] = (event_type == SDL_MOUSEBUTTONDOWN || event_type == SDL_MOUSEMOTION);
 }
 
-MOUSE_STATE ui_MouseButton(i32 mbutton)
+SDLUI_MOUSEBUTTON SDLUI_MouseButton(i32 mbutton)
 {
-    if(ui_base.mouse_current_frame[mbutton] && !ui_base.mouse_last_frame[mbutton])
+    if(SDLUI_Base.mouse_current_frame[mbutton] && !SDLUI_Base.mouse_last_frame[mbutton])
     {
-        return MOUSE_STATE::PRESSED;
+        return SDLUI_MOUSEBUTTON_PRESSED;
     }
-    else if(ui_base.mouse_current_frame[mbutton] && ui_base.mouse_last_frame[mbutton])
+    else if(SDLUI_Base.mouse_current_frame[mbutton] && SDLUI_Base.mouse_last_frame[mbutton])
     {
-        return MOUSE_STATE::HELD;
+        return SDLUI_MOUSEBUTTON_HELD;
     }
-    else if(!ui_base.mouse_current_frame[mbutton] && ui_base.mouse_last_frame[mbutton])
+    else if(!SDLUI_Base.mouse_current_frame[mbutton] && SDLUI_Base.mouse_last_frame[mbutton])
     {
-        return MOUSE_STATE::RELEASED;
+        return SDLUI_MOUSEBUTTON_RELEASED;
     }
     
-    return MOUSE_STATE::NONE;
+    return SDLUI_MOUSEBUTTON_NONE;
 }
 
-void ui_GradientToTexture(SDL_Texture *t, SDL_Color c, i32 width, i32 height, i32 interv)
+void SDLUI_GradientToTexture(SDL_Texture *t, SDL_Color c, i32 width, i32 height, i32 interv)
 {
-    SDL_SetRenderTarget(ui_base.renderer, t);
+    SDL_SetRenderTarget(SDLUI_Base.renderer, t);
     
     i32 x = 0, y = 0;
     
     for (int i = 0; i < height; ++i)
     {
-        SDL_SetRenderDrawColor(ui_base.renderer, c.r, c.g, c.b, 255);
-        SDL_RenderDrawLine(ui_base.renderer, x, y + i, width, y + i);
+        SDL_SetRenderDrawColor(SDLUI_Base.renderer, c.r, c.g, c.b, 255);
+        SDL_RenderDrawLine(SDLUI_Base.renderer, x, y + i, width, y + i);
         
         if(i % interv == 0)
         {
@@ -182,35 +219,118 @@ void ui_GradientToTexture(SDL_Texture *t, SDL_Color c, i32 width, i32 height, i3
         }
     }
     
-    SDL_SetRenderTarget(ui_base.renderer, NULL);
+    SDL_SetRenderTarget(SDLUI_Base.renderer, NULL);
 }
 
-//////////////////////////
-// Begin control creation
-/////////////////////////
-
-BUTTON *ui_CreateButton(char * caption, i32 x, i32 y)
+void SDLUI_SetColor(SDL_Color c)
 {
-    if(ui_base.count < MAX_CONTROLS)
+    SDL_SetRenderDrawColor(SDLUI_Base.renderer, c.r, c.g, c.b, c.a);
+}
+
+void SDLUI_Render()
+{
+    for (int i = 0; i < SDLUI_Base.count; ++i)
     {
-        CONTROL *ctrl = &ui_base.pool[ui_base.count];
-        ui_base.count++;
+        if(SDLUI_Base.pool[i].type == SDLUI_CONTROL_TYPE_BUTTON)
+        {
+            SDLUI_Control_Button *btn = &SDLUI_Base.pool[i].button;
+            
+            if(btn->render)
+            {
+                SDL_Rect r = {btn->x+1, btn->y+1, btn->w-2, btn->h-2};
+                SDL_RenderCopy(SDLUI_Base.renderer, btn->t_back, NULL, &r);
+                
+                r = {btn->x, btn->y, btn->w, btn->h};
+                SDLUI_SetColor(SDLUI_Base.theme.col_border);
+                SDL_RenderDrawRect(SDLUI_Base.renderer, &r);
+                
+                i32 w = SDLUI_Base.font.width * strlen(btn->caption);
+                i32 diffx = btn->w - w;
+                i32 diffy = (btn->h - SDLUI_Base.font.height)/2 + 1;
+                
+                r = {btn->x + btn->align * diffx/2, btn->y + diffy, w, SDLUI_Base.font.height};
+                SDL_RenderCopy(SDLUI_Base.renderer, btn->t_text, NULL, &r);
+            }
+        }
+        else if(SDLUI_Base.pool[i].type == SDLUI_CONTROL_TYPE_SLIDER_INT)
+        {
+            SDLUI_Control_SliderInt *si = &SDLUI_Base.pool[i].slider_int;
+            
+            if(si->render)
+            {
+                if(si->orientation == SDLUI_ORIENTATION_HORIZONTAL)
+                {
+                    i32 fill = (i32)SDLUI_Map(si->min, si->max, 0, si->w, si->value);
+                    i32 empty = si->w - fill;
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_highlight);
+                    SDL_Rect r = {si->x, si->y, fill, si->h};
+                    SDL_RenderFillRect(SDLUI_Base.renderer, &r);
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_border);
+                    r = {si->x + fill, si->y, empty, si->h};
+                    SDL_RenderFillRect(SDLUI_Base.renderer, &r);
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_thumb);
+                    r = {si->x + fill, si->y, si->thumb_size, si->h};
+                    r.x = SDLUI_Clamp(r.x, si->x, si->x + si->w - si->thumb_size);
+                    SDL_RenderFillRect(SDLUI_Base.renderer, &r);
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_border);
+                    SDL_RenderDrawRect(SDLUI_Base.renderer, &r);
+                }
+                else
+                {
+                    i32 fill = (i32)SDLUI_Map(si->min, si->max, 0, si->h, si->value);
+                    i32 empty = si->h - fill;
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_border);
+                    SDL_Rect r = {si->x, si->y, si->w, empty};
+                    SDL_RenderFillRect(SDLUI_Base.renderer, &r);
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_highlight);
+                    r = {si->x, si->y + empty, si->w, fill};
+                    SDL_RenderFillRect(SDLUI_Base.renderer, &r);
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_thumb);
+                    r = {si->x, si->y + empty, si->w, si->thumb_size};
+                    r.y = SDLUI_Clamp(r.y, si->y, si->y + si->h - si->thumb_size);
+                    SDL_RenderFillRect(SDLUI_Base.renderer, &r);
+                    
+                    SDLUI_SetColor(SDLUI_Base.theme.col_border);
+                    SDL_RenderDrawRect(SDLUI_Base.renderer, &r);
+                }
+            }
+        }
+    }
+    
+    SDLUI_MouseStateReset();
+}
+
+// Controls ---------------------------------------------------
+
+SDLUI_Control_Button *SDLUI_CreateButton(char * caption, i32 x, i32 y)
+{
+    if(SDLUI_Base.count < SDLUI_MAX_CONTROLS)
+    {
+        SDLUI_Control *ctrl = &SDLUI_Base.pool[SDLUI_Base.count];
+        SDLUI_Base.count++;
         
-        ctrl->type = CONTROL_TYPE::Button;
+        ctrl->type = SDLUI_CONTROL_TYPE::SDLUI_CONTROL_TYPE_BUTTON;
         ctrl->button.caption = caption;
         ctrl->button.x = x;
         ctrl->button.y = y;
         ctrl->button.w = 160;
         ctrl->button.h = 30;
         ctrl->button.render = false;
-        ctrl->button.align = ALIGN::CENTER;
+        ctrl->button.align = SDLUI_ALIGN_CENTER;
         
         SDL_Color c = {255, 255, 255, 255};
-        SDL_Surface *s = TTF_RenderText_Blended(ui_base.font.handle,caption, c);
-        ctrl->button.t_text = SDL_CreateTextureFromSurface(ui_base.renderer, s);
-        ctrl->button.t_back = SDL_CreateTexture(ui_base.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, ctrl->button.w - 2, ctrl->button.h - 2);
+        SDL_Surface *s = TTF_RenderText_Blended(SDLUI_Base.font.handle,caption, c);
+        ctrl->button.t_text = SDL_CreateTextureFromSurface(SDLUI_Base.renderer, s);
+        ctrl->button.t_back = SDL_CreateTexture(SDLUI_Base.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, ctrl->button.w - 2, ctrl->button.h - 2);
         
-        ui_GradientToTexture(ctrl->button.t_back, ui_base.theme.col_base, ctrl->button.w-2, ctrl->button.h-2, (ctrl->button.h-2)/12);
+        SDLUI_GradientToTexture(ctrl->button.t_back, SDLUI_Base.theme.col_base, ctrl->button.w-2, ctrl->button.h-2, (ctrl->button.h-2)/12);
         
         return &ctrl->button;
     }
@@ -218,21 +338,21 @@ BUTTON *ui_CreateButton(char * caption, i32 x, i32 y)
     return NULL;
 }
 
-bool ui_Button(BUTTON *btn)
+bool SDLUI_Button(SDLUI_Control_Button *btn)
 {
     btn->render = true;
     i32 mx, my;
     SDL_GetMouseState(&mx, &my);
     
     SDL_Rect r = {btn->x,btn->y,btn->w,btn->h};
-    if(ui_PointCollision(r, mx, my))
+    if(SDLUI_PointCollision(r, mx, my))
     {
-        if(ui_MouseButton(SDL_BUTTON_LEFT) == MOUSE_STATE::PRESSED)
+        if(SDLUI_MouseButton(SDL_BUTTON_LEFT) == SDLUI_MOUSEBUTTON_PRESSED)
         {
             SDL_SetTextureColorMod(btn->t_back, 100, 100, 100);
             return true;
         }
-        if(ui_MouseButton(SDL_BUTTON_LEFT) == MOUSE_STATE::HELD)
+        if(SDLUI_MouseButton(SDL_BUTTON_LEFT) == SDLUI_MOUSEBUTTON_HELD)
         {
             SDL_SetTextureColorMod(btn->t_back, 100, 100, 100);
         }
@@ -248,21 +368,34 @@ bool ui_Button(BUTTON *btn)
     return false;
 }
 
-SLIDER_INT *ui_CreateSliderInt(i32 x, i32 y, i32 min, i32 max, i32 value)
+SDLUI_Control_SliderInt *SDLUI_CreateSliderInt(i32 x, i32 y, i32 min, i32 max, i32 value, SDLUI_ORIENTATION orientation = SDLUI_ORIENTATION_HORIZONTAL)
 {
-    if(ui_base.count < MAX_CONTROLS)
+    if(SDLUI_Base.count < SDLUI_MAX_CONTROLS)
     {
-        CONTROL *ctrl = &ui_base.pool[ui_base.count];
-        ui_base.count++;
+        SDLUI_Control *ctrl = &SDLUI_Base.pool[SDLUI_Base.count];
+        SDLUI_Base.count++;
         
-        ctrl->type = CONTROL_TYPE::SliderInt;
+        ctrl->type = SDLUI_CONTROL_TYPE_SLIDER_INT;
         ctrl->slider_int.x = x;
         ctrl->slider_int.y = y;
-        ctrl->slider_int.w = 50;
-        ctrl->slider_int.h = 10;
         ctrl->slider_int.min = min;
         ctrl->slider_int.max = max;
         ctrl->slider_int.value = value;
+        ctrl->slider_int.thumb_size = 10;
+        ctrl->slider_int.render = false;
+        ctrl->slider_int.ischanging = false;
+        ctrl->slider_int.orientation = orientation;
+        
+        if(ctrl->slider_int.orientation == SDLUI_ORIENTATION_HORIZONTAL)
+        {
+            ctrl->slider_int.w = 100;
+            ctrl->slider_int.h = 10;
+        }
+        else
+        {
+            ctrl->slider_int.w = 10;
+            ctrl->slider_int.h = 100;
+        }
         
         return &ctrl->slider_int;
     }
@@ -270,44 +403,52 @@ SLIDER_INT *ui_CreateSliderInt(i32 x, i32 y, i32 min, i32 max, i32 value)
     return NULL;
 }
 
-void ui_SetColor(SDL_Color c)
+bool SDLUI_SliderInt(SDLUI_Control_SliderInt *si)
 {
-    SDL_SetRenderDrawColor(ui_base.renderer, c.r, c.g, c.b, c.a);
-}
-
-void ui_Render()
-{
-    for (int i = 0; i < ui_base.count; ++i)
+    si->render = true;
+    i32 mx, my;
+    SDL_GetMouseState(&mx, &my);
+    
+    SDL_Rect r = {si->x,si->y,si->w,si->h};
+    if(SDLUI_PointCollision(r, mx, my))
     {
-        if(ui_base.pool[i].type == CONTROL_TYPE::Button)
+        if(SDLUI_MouseButton(SDL_BUTTON_LEFT) == SDLUI_MOUSEBUTTON_PRESSED)
         {
-            BUTTON *btn = &ui_base.pool[i].button;
-            
-            if(btn->render)
+            if(si->orientation == SDLUI_ORIENTATION_HORIZONTAL)
             {
-                SDL_Rect r = {btn->x, btn->y, btn->w, btn->h};
-                SDL_Rect testrect = {btn->x+1, btn->y+1, btn->w-2, btn->h-2};
-                SDL_RenderCopy(ui_base.renderer, btn->t_back, NULL, &testrect);
-                
-                ui_SetColor(ui_base.theme.col_border);
-                SDL_RenderDrawRect(ui_base.renderer, &r);
-                
-                i32 w = ui_base.font.width * strlen(btn->caption);
-                i32 diffx = btn->w - w;
-                i32 diffy = (btn->h - ui_base.font.height)/2 + 1;
-                
-                r = {btn->x + btn->align * diffx/2, btn->y + diffy, w, ui_base.font.height};
-                SDL_RenderCopy(ui_base.renderer, btn->t_text, NULL, &r);
+                si->value = SDLUI_Map(si->x, si->x + si->w, si->min, si->max, mx);
             }
-        }
-        else if(ui_base.pool[i].type == CONTROL_TYPE::SliderInt)
-        {
-            SLIDER_INT *si = &ui_base.pool[i].slider_int;
-            SDL_SetRenderDrawColor(ui_base.renderer, 255, 0, 0, 255);
-            SDL_Rect r = {si->x, si->y, si->w, si->h};
-            SDL_RenderDrawRect(ui_base.renderer, &r);
+            else
+            {
+                si->value = SDLUI_Map(si->y + si->h, si->y, si->min, si->max, my);
+            }
+            
+            si->ischanging = true;
+            return true;
         }
     }
     
-    ui_MouseStateReset();
+    if(SDLUI_MouseButton(SDL_BUTTON_LEFT) == SDLUI_MOUSEBUTTON_HELD && si->ischanging)
+    {
+        if(si->orientation == SDLUI_ORIENTATION_HORIZONTAL)
+        {
+            si->value = SDLUI_Map(si->x, si->x + si->w, si->min, si->max, mx);
+            si->value = SDLUI_Clamp(si->value, si->min, si->max);
+        }
+        else
+        {
+            si->value = SDLUI_Map(si->y + si->h, si->y, si->min, si->max, my);
+            si->value = SDLUI_Clamp(si->value, si->min, si->max);
+        }
+        
+        return true;
+    }
+    
+    if(SDLUI_MouseButton(SDL_BUTTON_LEFT) == SDLUI_MOUSEBUTTON_RELEASED)
+    {
+        si->ischanging = false;
+        return false;
+    }
+    
+    return false;
 }
