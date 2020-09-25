522940df07abc3024d7314097f62f2b14ccbcacb
immortalx74 <jfdodis@yahoo.com>
2020-09-25 19:12:32 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -6,10 +6,10 @@ set SOURCES=main.cpp
 set LINK=opengl32.lib winmm.lib gdi32.lib shell32.lib lib\SDL2.lib lib\SDL2main.lib lib\SDL2_ttf.lib lib\SDL2_image.lib
 
 REM DEBUG
-REM set CFLAGS=/MP /MTd /EHsc /Zi /FC /nologo /W1 /Od
+set CFLAGS=/MP /MTd /EHsc /Zi /FC /nologo /W1 /Od
 
 REM RELEASE
-set CFLAGS=/MP /MT /EHsc /FC /nologo /W1 /O2
+REM set CFLAGS=/MP /MT /EHsc /FC /nologo /W1 /O2
 
 set BUILD_DIR=/Fdbuild\ /Fobuild\
 set EXE=build/sdlui_test.exe
